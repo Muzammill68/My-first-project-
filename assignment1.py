1c93afa7283a862782de3ac03a4d315d1c2fcecc
muzammil yaseen <muzammilhyaseen@gmail.com>
2026-06-10 03:20:59 +0500

    assignment4.py updated

--- assignment1.py
+++
@@ -1,9 +0,0 @@
-print(("Muzammil yassen" + "\n") * 10)
-a= 20
-b = "muzammil python"
-print(type(b))
-
-x = "muzammil is working on the python"
-def myfunc():
-    print(x)
-    myfunc()
---
+++ assignment4.py
@@ -0,0 +1,13 @@
+#convert your feelings u will put into emojis
+msg = input("Enter your text: ")
+msg = msg.replace("im happy", "😊")
+msg = msg.replace("im sad", "😢")
+msg = msg.replace("im angry", "😠")
+msg = msg.replace("i love you", "❤️")
+msg = msg.replace("im tired", "😴")
+print(msg)
+
+x = "Hello"
+y = "World"
+z = x + " " + y
+print(z)

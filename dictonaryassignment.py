a35435ec1d041e724a9c8240c0ecf6e282cb2335
muzammil yaseen <muzammilhyaseen@gmail.com>
2026-06-11 03:01:10 +0500

    update dictonaryasssignment

---
+++ dictonaryassignment.py
@@ -0,0 +1,9 @@
+#create a dictonary named marks to store marks of 5 subjects and add the subjects ane by one and print the final dictonary
+marks = {}
+marks["Math"] = 85
+marks["Science"] = 90
+marks["English"] = 88
+marks["History"] = 92
+marks["Geography"] = 87
+print(marks)
+print(type(marks))# u can see in the result its type is dictonary.

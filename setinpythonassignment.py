40f4bc1dbd8d5d848907a6010a16e709f2834deb
muzammil yaseen <muzammilhyaseen@gmail.com>
2026-06-11 03:20:45 +0500

    inti

---
+++ setinpythonassignment.py
@@ -0,0 +1,6 @@
+#you are given a list  of programming language
+languages = ["Python", "Java", "C++", "JavaScript", "Ruby"]
+#convert the list into a set and print how many unique programming languages are muzammil knows.
+unique_languages = set(languages)
+print("Unique programming languages:", unique_languages)
+print("Number of unique programming languages Muzammil knows:", len(unique_languages))

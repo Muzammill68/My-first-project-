10613e9ade0b3008812df27f2c5381687166c770
muzammil yaseen <muzammilhyaseen@gmail.com>
2026-06-11 05:49:44 +0500

    init

---
+++ forloop.py
@@ -0,0 +1,17 @@
+#now we are learning the for loops we use for loop for list tupple or strings 
+# question 1 is 
+fruitlist = ["Mangoes", "Banana", "orange"] #this is list
+for item in fruitlist :
+    print(item)
+
+   
+    collegelist = ("Ahmed school" , "Ali school" , "Muzammil school" , "Noor school") #this is tuple
+    for i in collegelist:
+     print("colleges visted by Muzammil yaseen ", i)
+     
+
+     #Most important for loop used with range() #this is range function and it genrates the sequence of number often used with loops
+     #syntax range(start, stop , step) step is increment is increment by default 1 ham is ko zada b rkh skty agr ham ny jump zada krwana ho to 
+    
+    
+
